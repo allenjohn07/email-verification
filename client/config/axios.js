315cf75a79aef094962019017ed9642f9b0581cc
Allen John <allenjohnmonapallil@gmail.com>
2024-07-04 13:40:30 +0530

    updated port for deploy

--- client/config/axios.js
+++ client/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:6500',
+    // baseURL: 'http://localhost:6500',
+    baseURL: 'https://email-verification-server.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
